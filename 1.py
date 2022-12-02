30aac364647cce69ed1ff6c6a0a81ba0c582ec12
master71512 <113140230+master71512@users.noreply.github.com>
2022-12-02 16:33:50 +0300

    1 done

---
+++ 1.py
@@ -0,0 +1,15 @@
+# Вычислить число Pi c заданной точностью d, не используя ф. round()              
+
+from math import pi
+
+d = float(input('введите d: 10^(-1) ≤ d ≤10^(-10)\n'))
+k = 0
+while d < 1:
+    d *= 10
+    k += 1
+res = int(pi * (10**(k+1)))
+if res % 10 >= 5:
+    res = (res //10 + 1)*(10**(-k))
+else:
+    res = res //10 * (10**(-k))
+print(res)
